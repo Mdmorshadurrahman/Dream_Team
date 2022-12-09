0228d3921fd699ba0e248ab991937994ac913c6c
Mdmorshadurrahman <mdmorshadur.rahman@udo.edu>
2022-12-09 03:47:11 +0100

    50% javascript work done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-document.getElementById('btn-hire').addEventListener('click', function () {
+
+const selectionBoard = (id) => {
     document.getElementById('balance-id').style.color = "lightgreen";
     document.getElementById('forward-id').style.color = "lightgreen";
     document.getElementById('total-id').style.color = "red";
-    const Value = getValueFromButton('price-id')
-    const Name = getInfosFromId('name-id', 'position-id')[0];
-    const Position = getInfosFromId('name-id', 'position-id')[1];
+    const Value = getValueFromButton('price-id' + id)
+    const Name = getInfosFromId('name-id' + id, 'position-id' + id)[0];
+    const Position = getInfosFromId('name-id' + id, 'position-id' + id)[1];
     setInfoIntoList(Name, 'name-list', Position, 'position-list');
     setValueintoWallet(Value, 'balance-id', 'forward-id', 'total-id');
-})
+
+}
